f2d4180460fe02ec8e0bcc72c4a603fb93ec7961
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-02-03 15:01:03 -0600

    separtated storing and printing vector into methods

--- HW1.cpp
+++ HW1.cpp
@@ -8,25 +8,29 @@ using namespace std;
 //Operating Systems
 //1 Feb 2017
 
-int main( ) {
-
-   int c;
-   string data;
-   string duration;
-   vector<pair<string, string> > processes;
-   //make sure last line in file is not empty
-   while(!cin.eof()){
-   	getline(cin, data, ' ');
-   	getline(cin, duration);
-   	processes.push_back(make_pair(data, duration));
-   	
-   }
-   for(int i = 0; i < processes.size(); i++)
-{
-     cout << processes[i].first << ", " << processes[i].second << endl;
+string data;
+string duration;
+vector<pair<string, string> > processes;
+/* Reads file and stores it into a vector of pairs. Does not take any arguments. Does not output or return anything. */
+void readFile() {
+	//make sure last line in file is not empty
+	while (!cin.eof()) {
+		getline(cin, data, ' ');
+		getline(cin, duration);
+		processes.push_back(make_pair(data, duration));
+	}
 }
+/* Prints processes with their durations. Does not take any arguments. Does not return anything. */
+void printProcesses(){
+	for (int i = 0; i < processes.size(); i++) {
+		cout << processes[i].first << ", " << processes[i].second << endl;
+	}
+}
+int main( ) {
+	readFile();
+	printProcesses();
 
-   
 
-   return 0;
+
+	return 0;
 }
