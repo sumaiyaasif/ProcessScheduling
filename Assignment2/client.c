17dd39f3ea09d064eb397d804b8820d10aea185e
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-03-20 10:09:05 -0500

    asks for input instead of arguments

--- Assignment2/client.c
+++ Assignment2/client.c
@@ -18,17 +18,21 @@ int main(int argc, char *argv[])
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
+    char hostRequested[32];
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    // if (argc < 3) {
+    //    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    //    exit(0);
+    // }
+    printf("Enter server host name: ");
+    scanf("%s", hostRequested);
+    printf("Enter server port number: ");
+    scanf("%d", &portno);
+    //portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    server = gethostbyname(hostRequested);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
@@ -41,7 +45,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
+    printf("Enter a user name: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
--- Assignment2/server.c
+++ Assignment2/server.c
@@ -16,7 +16,7 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+     int sockfd, newsockfd;
      socklen_t clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
@@ -24,19 +24,19 @@ int main(int argc, char *argv[])
      int portNumber;
      printf("Enter server port number: ");
      scanf("%d", &portNumber);
-     printf("\nYou entered: %d", portNumber);
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+
+     // if (argc < 2) {
+     //     fprintf(stderr,"ERROR, no port provided\n");
+     //     exit(1);
+     // }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+     //portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+     serv_addr.sin_port = htons(portNumber);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
