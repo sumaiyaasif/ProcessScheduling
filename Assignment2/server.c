95580e5a258df860a00706da24f3b96bb4b4584b
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-03-22 13:01:22 -0500

    back to sample program

--- Assignment2/server.c
+++ Assignment2/server.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -16,7 +16,7 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+     int sockfd, newsockfd;
      socklen_t clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
@@ -24,28 +24,28 @@ int main(int argc, char *argv[])
      int portNumber;
      printf("Enter server port number: ");
      scanf("%d", &portNumber);
-     printf("\nYou entered: %d", portNumber);
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+
+     // if (argc < 2) {
+     //     fprintf(stderr,"ERROR, no port provided\n");
+     //     exit(1);
+     // }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+     //portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+     serv_addr.sin_port = htons(portNumber);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
+     newsockfd = accept(sockfd,
+                 (struct sockaddr *) &cli_addr,
                  &clilen);
-     if (newsockfd < 0) 
+         if (newsockfd < 0)
           error("ERROR on accept");
      bzero(buffer,256);
      n = read(newsockfd,buffer,255);
@@ -55,5 +55,5 @@ int main(int argc, char *argv[])
      if (n < 0) error("ERROR writing to socket");
      close(newsockfd);
      close(sockfd);
-     return 0; 
+     return 0;
 }
