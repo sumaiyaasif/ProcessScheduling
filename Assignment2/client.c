a62391fa47ccb33883c1164278620334bfc1de5c
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-03-22 21:35:44 -0500

    inputs working now working on sending keys back to client

--- Assignment2/client.c
+++ Assignment2/client.c
@@ -18,17 +18,40 @@ int main(int argc, char *argv[])
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char userNameRequested[256];
+    char portNumber[8];
+    char serverName[9];
+    // if (argc < 3) {
+    //    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    //    exit(0);
+    // }
+    // portno = atoi(argv[2]);
+    printf("Please enter server name: ");
+    bzero(serverName, 9);
+    bzero(portNumber, 8);
+    fgets(serverName, 11, stdin);
+    printf("This is your server name: %s\n", serverName);
+    
+    printf("Please enter port number: ");
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    fgets(portNumber, 7, stdin);
+
+    portno = atoi(portNumber);
+    printf("This is your port number: %s\n", portNumber);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    
+    char sname[10];
+    memcpy(sname, &serverName[0], 9);
+    sname[9] = '\0';
+    server = gethostbyname(sname);
+
+    if(!server){
+        printf("Not a valid server");
+        exit(0);
+    }
+    printf("Name: %s\n", server->h_name);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
@@ -41,17 +64,17 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
+    printf("Please enter the username: ");
+    bzero(userNameRequested,256);
+    fgets(userNameRequested,255,stdin);
+    n = write(sockfd,userNameRequested,strlen(userNameRequested));
     if (n < 0)
          error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+    bzero(userNameRequested,256);
+    n = read(sockfd,userNameRequested,255);
     if (n < 0)
          error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    printf("%s\n",userNameRequested);
     close(sockfd);
     return 0;
 }
--- Assignment2/server.c
+++ Assignment2/server.c
@@ -18,22 +18,17 @@ int main(int argc, char *argv[])
 {
      int sockfd, newsockfd;
      socklen_t clilen;
-     char buffer[256];
+     char userName[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
      int portNumber;
      printf("Enter server port number: ");
      scanf("%d", &portNumber);
 
-     // if (argc < 2) {
-     //     fprintf(stderr,"ERROR, no port provided\n");
-     //     exit(1);
-     // }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     //portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portNumber);
@@ -47,10 +42,15 @@ int main(int argc, char *argv[])
                  &clilen);
          if (newsockfd < 0)
           error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
+     bzero(userName,256);
+     n = read(newsockfd,userName,255);
      if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
+     printf("Here is the user name: %s\n",userName);
+     int isEqual = strncmp(userName, "jim", 3);
+     printf("equal %i", isEqual);
+     if(strncmp(userName, "jim", 3) == 0){
+        printf("jim found");
+     }
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
      close(newsockfd);
