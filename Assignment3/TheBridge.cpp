4d8389ed35b814cdf88ffd3ae5dba71784434a07
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-04-26 14:31:03 -0500

    done with logic. Should re-check

--- Assignment3/TheBridge.cpp
+++ Assignment3/TheBridge.cpp
@@ -9,21 +9,13 @@
 
 using namespace std;
 
-#define NUM_THREADS	1
+#define NUM_THREADS	5
 
 int static maxBridgeLoad = 10;
 int static currectBridgeLoad = 0;
 
 static pthread_mutex_t mylock;
 pthread_cond_t ok = PTHREAD_COND_INITIALIZER;
-//unsigned int sleep(unsigned int seconds);
-
-string licensePlate;
-int arrivalTimeInt;
-int vehicleWeightInt;
-int timeToCrossInt;
-
-struct thread_data *my_data;
 
 struct thread_data{
    string  thread_id;
@@ -32,60 +24,58 @@ struct thread_data{
    int timeToCrossInt;
 };
 
-void enterBridge(int vehicleWeightInt){
-	currectBridgeLoad = currectBridgeLoad + vehicleWeightInt;
+void enterBridge(struct thread_data *my_data){
+	pthread_mutex_lock(&mylock);
+	currectBridgeLoad = currectBridgeLoad + my_data->vehicleWeightInt;
 	cout << "Vehicle: " << my_data->thread_id << " is CROSSING the bridge. Current bridge load is: " << currectBridgeLoad << endl;
+	pthread_mutex_unlock(&mylock);
 }
 
-void leaveBridge(int vehicleWeightInt){
-	currectBridgeLoad = currectBridgeLoad - vehicleWeightInt;
+void leaveBridge(struct thread_data *my_data){
+	pthread_mutex_lock(&mylock);
+	currectBridgeLoad = currectBridgeLoad - my_data->vehicleWeightInt;
 	cout << "Vehicle: " << my_data->thread_id << " is LEAVING the bridge. Current bridge load is: " << currectBridgeLoad << endl;
+	pthread_cond_signal(&ok);
+	pthread_mutex_unlock(&mylock);
 }
 
 void *PrintHello(void *threadarg) {
    
-
-   my_data = (struct thread_data *) threadarg;
+   struct thread_data *my_data = (struct thread_data *) threadarg;
 
    cout << "Vehicle: " << my_data->thread_id << " has ARRIVED at bridge. Current bridge load is: " << currectBridgeLoad << endl;
-   // If bridge load + vehichle weight <= maxBridgeLoad
-   		// enterBridge(weight);
-   		// sleep(timeToCrossInt);
-   		// leaveBridge(weight);
-   // If vehicle weight > maxBridgeLoad
-   		// Vehicle will not cross and thread will exit
-   // Else
-   		// Vehicle will wait before crossing
-   		// pthread_mutex_lock(&access);
-   		//	while(vehicle weight > bridge load + vehicle weight)
-   		//		pthread_cond_wait(&ok, &access);
-   		// pthread_mutex_unlock(&access);
-   if(currectBridgeLoad + vehicleWeightInt <= maxBridgeLoad){
-   		enterBridge(vehicleWeightInt);
-   		sleep(timeToCrossInt);
-   		leaveBridge(vehicleWeightInt);
+   
+   // Vehicle will NOT be allowed on the bridge ever
+   if(my_data->vehicleWeightInt > maxBridgeLoad){
+   		cout << "Vehicle: " << my_data->thread_id << " exceeds the max bridge hold. It will not be allowed to cross." << endl;
+   		pthread_exit(NULL);
    }
-   else if(vehicleWeightInt > maxBridgeLoad){
-   		//
+   // Vehicle will be allowed onto the bridge after some wait time
+   pthread_mutex_lock(&mylock);
+   while(my_data->vehicleWeightInt + currectBridgeLoad > maxBridgeLoad){
+   		pthread_cond_wait(&ok, &mylock);
    }
-   else{
-   		pthread_mutex_lock(&mylock);
-   		while(vehicleWeightInt > currectBridgeLoad + vehicleWeightInt){
-   			pthread_cond_wait(&ok, &mylock);
-   		}
-   		pthread_mutex_unlock(&mylock);
-   }
+   pthread_mutex_unlock(&mylock);
+   pthread_cond_signal(&ok);
+   // Vehicle will be allowed onto the bridge immediately
+   enterBridge(my_data);
+   sleep(my_data->timeToCrossInt);
+   leaveBridge(my_data);
+
    pthread_exit(NULL);
 }
 
 void readFile(){
 	pthread_t threads[NUM_THREADS];
 	struct thread_data td[NUM_THREADS];
-
+	string licensePlate;
+	int arrivalTimeInt;
+	int vehicleWeightInt;
+	int timeToCrossInt;
 	string arrivalTime;
 	string vehicleWeight;
 	string timeToCross;	
-	ifstream infile("sample.txt");
+	ifstream infile("sample3.txt");
 
 	for (int i = 0; i < NUM_THREADS; i++){
 
@@ -98,32 +88,30 @@ void readFile(){
 	infile >> ws;
 	getline(infile, timeToCross);
 
-	arrivalTimeInt = atoi(arrivalTime.c_str());
-	vehicleWeightInt = atoi(vehicleWeight.c_str());
-	timeToCrossInt = atoi(timeToCross.c_str());
-	
-	sleep(arrivalTimeInt);
+	td[i].arrivalTimeInt = atoi(arrivalTime.c_str());
+	td[i].vehicleWeightInt = atoi(vehicleWeight.c_str());
+	td[i].timeToCrossInt = atoi(timeToCross.c_str());
+	td[i].thread_id = licensePlate;
+
+	sleep(td[i].arrivalTimeInt);
 	 
-	cout << "License Plate: " << licensePlate << " Arrival Time: " << arrivalTimeInt << " Weight: " << vehicleWeightInt << " Time To Cross: " << timeToCrossInt << endl;
+	cout << "License Plate: " << td[i].thread_id << " Arrival Time: " << td[i].arrivalTimeInt << " Weight: " << td[i].vehicleWeightInt << " Time To Cross: " << td[i].timeToCrossInt << endl;
 
 	int rc;
 
-       td[i].thread_id = licensePlate;
-      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
-      pthread_join(threads[i], NULL);
-      if (rc){
-         cout << "Error:unable to create thread," << rc << endl;
-         exit(-1);
+    rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
+    if (rc){
+    	cout << "Error:unable to create thread," << rc << endl;
+        exit(-1);
       }
 	}
-	// pthread_create(%licensePlate, NULL, vehicle, (void *)&td);
+	for (int i = 0; i < NUM_THREADS; i++)
+	 	pthread_join(threads[i], NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	
 	pthread_mutex_init(&mylock, NULL);
-
 	readFile();
     pthread_exit(NULL);
 }
